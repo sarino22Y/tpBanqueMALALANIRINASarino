9f607909abb221701f887e6b4785eb8e4d6db703
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2023-04-05 23:31:54 +0300

    Écriture d'un EJB session façade, GestionnaireCompte

---
+++ src/main/java/mg/itu/tpbanquemalalanirinasarino/ejb/GestionnaireCompte.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquemalalanirinasarino.ejb;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import java.util.List;
+import mg.itu.tpbanquemalalanirinasarino.entities.CompteBancaire;
+
+/**
+ *
+ * @author Sarino
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="sarino", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
+@Stateless
+public class GestionnaireCompte {
+    
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+    
+    public List<CompteBancaire> getAllCustomers() {
+       TypedQuery<CompteBancaire> query = (TypedQuery<CompteBancaire>) em.createNamedQuery("CompteBancaire.findAll");
+       return query.getResultList();
+    }
+
+    public void creerCompte(CompteBancaire c) {
+       em.persist(c);
+    }
+    
+}
