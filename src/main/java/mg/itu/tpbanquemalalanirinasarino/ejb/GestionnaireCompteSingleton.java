c3b6e73af90040a02f027985b428bb50a4e86835
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2023-04-06 17:24:27 +0300

    Singleton Init

---
+++ src/main/java/mg/itu/tpbanquemalalanirinasarino/ejb/GestionnaireCompteSingleton.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquemalalanirinasarino.ejb;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.EJB;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+import mg.itu.tpbanquemalalanirinasarino.entities.CompteBancaire;
+
+/**
+ *
+ * @author Sarino
+ */
+@Singleton
+@Startup
+public class GestionnaireCompteSingleton {
+
+    @EJB
+    private GestionnaireCompte gCompte;
+
+    @PostConstruct
+    public void init() {
+        createAccounts();
+    }
+    
+    public void createAccounts() {
+        CompteBancaire compte1 = new CompteBancaire("John Lennon", 150000);
+        CompteBancaire compte2 = new CompteBancaire("Paul McCartney", 950000);
+        CompteBancaire compte3 = new CompteBancaire("Ringo Starr", 20000);
+        CompteBancaire compte4 = new CompteBancaire("Georges Harrisson", 100000);
+
+        gCompte.creerCompte(compte1);        
+        gCompte.creerCompte(compte2);
+        gCompte.creerCompte(compte3);
+        gCompte.creerCompte(compte4);
+    }
+}
